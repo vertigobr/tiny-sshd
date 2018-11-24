a7386357f6c65c5fb628d6253cb96f9e99dbc884
Andre Fernandes <andre@vertigo.com.br>
2018-11-24 15:12:28 -0200

    script vars

--- sshd.sh
+++ sshd.sh
@@ -4,12 +4,14 @@ IMGVERSION=${IMGVERSION:-"latest"}
 IMGNAME=$(head -n 1 .IMGNAME)
 ENVBASH=$1
 ENVBASH=${ENVBASH:-"sh"}
+KEYONLY=${KEYONLY:-"true"}
 PUBLICKEY=`cat ~/.ssh/id_rsa.pub`
 docker stop sshd
 docker rm sshd
 docker run --name sshd -d \
   -p 2200:22 \
   -e "PUBLICKEY=$PUBLICKEY" \
-  -e "KEYONLY=true" \
+  -e "KEYONLY=$KEYONLY" \
   -e "HOSTCHECK=false" \
+  -e "USERPWD=$USERPWD" \
   "$IMGNAME:$IMGVERSION"
