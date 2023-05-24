15e1758c462bba5bb6eeca02d63d13a569fa4c7f
Andre Fernandes <andre@vertigo.com.br>
2023-05-24 03:11:40 -0300

    test script

--- sshd.sh
+++ sshd.sh
@@ -4,8 +4,8 @@ IMGVERSION=${IMGVERSION:-"latest"}
 IMGNAME=$(head -n 1 .IMGNAME)
 ENVBASH=$1
 ENVBASH=${ENVBASH:-"sh"}
-#KEYONLY=${KEYONLY:-"true"}
-PUBLICKEY=`cat ~/.ssh/id_rsa.pub`
+KEYONLY=${KEYONLY:-"true"}
+#PUBLICKEY=`cat ~/.ssh/id_rsa.pub`
 PUBLICKEY=`cat ~/.ssh/id_ed25519.pub`
 docker stop sshd
 docker rm sshd
